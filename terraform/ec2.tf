4cfae56e21ef85c3cec7e5c5e6525bbc04fe8d80
Adit Modi <adit.modi24@gmail.com>
2020-09-23 19:06:44 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids      = [aws_security_group.security_group.id]
   associate_public_ip_address = true
 
+  
   key_name = aws_key_pair.key_pair.key_name
 
   user_data = file("nginx.sh")
